a2d87cac2c2f02716d4087dc657b49338dcc8497
DanielleInkster <danielle.inkster@gmail.com>
2019-11-20 13:55:25 +0000

    Refactored practice file to work

--- ruby/texttest_fixture.rb
+++ ruby/texttest_fixture.rb
@@ -1,4 +1,6 @@
-require_relative  './lib/gilded_rose'
+#!/usr/bin/ruby -w
+
+require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 puts "OMGHAI!"
 items = [
@@ -19,12 +21,12 @@ if ARGV.size > 0
   days = ARGV[0].to_i + 1
 end
 
-gilded_rose = GildedRose.new(items)
+gilded_rose = GildedRose.new items
 (0...days).each do |day|
   puts "-------- day #{day} --------"
   puts "name, sellIn, quality"
   items.each do |item|
-    puts "#{item.name}, #{item.sell_in}, #{item.quality}" 
+    puts item
   end
   puts ""
   gilded_rose.update_quality
