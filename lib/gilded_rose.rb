9c7863cc6233a2528ddbeea8bb3904855c154a8c
DanielleInkster <danielle.inkster@gmail.com>
2019-11-20 14:18:49 +0000

    Set minimum quality to 0

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,6 +2,8 @@ require_relative 'item'
 
 class GildedRose
 
+  MINIMUM_QUALITY = 0
+
   def initialize(items)
     @items = items
   end
@@ -9,7 +11,11 @@ class GildedRose
   def update_quality
     @items.each do |item|
       item.sell_in -= 1
+      if item.quality ===  MINIMUM_QUALITY
+        MINIMUM_QUALITY
+      else
       item.quality -= 1
+      end
     end
   end
 
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,16 +4,22 @@ describe GildedRose do
 
   describe "#update_quality" do
     it 'subtracts one from the sell_in day'do
-    items = [Item.new("foo", 1, 1)]
+      items = [Item.new("foo", 1, 1)]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq 0
     end
 
     it 'subtracts one from the quality count'do
-    items = [Item.new("foo", 1, 1)]
+      items = [Item.new("foo", 1, 1)]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 0
     end
+
+    it 'does not put quality below 0' do
+    items = [Item.new("foo", 0, 0)]
+    GildedRose.new(items).update_quality()
+    expect(items[0].quality).to eq 0
+  end
 
 
 
