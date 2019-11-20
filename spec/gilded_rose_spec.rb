d3c087ce4494f7076a7adfedcdaa6600b2fdf7c9
DanielleInkster <danielle.inkster@gmail.com>
2019-11-20 14:13:17 +0000

    Setup basic day subtraction

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,11 +3,20 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+    it 'subtracts one from the sell_in day'do
+    items = [Item.new("foo", 1, 1)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      expect(items[0].sell_in).to eq 0
     end
+
+
+
+    # it "does not allow quality to fall below 0" do
+    #   items = [Item.new("foo", 0, 0)]
+    #   GildedRose.new(items).update_quality()
+    #   expect(items[0].quality).to eq 0
+
+
   end
 
 end
