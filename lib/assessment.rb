1f57a9a735d716e75146819c6ebdce0348fb2328
DanielleInkster <danielle.inkster@gmail.com>
2019-11-20 23:09:48 +0000

    Extracted assessment logic to new class

---
+++ lib/assessment.rb
@@ -0,0 +1,58 @@
+require_relative 'item'
+require_relative 'gilded_rose'
+
+class ItemAssessor
+  SULFURAS_MAX = 80
+  GEN_MAX = 50
+  GEN_MIN = 0
+
+  def initialize(special_items = ['Aged Brie',
+    'Backstage passes to a TAFKAL80ETC concert',
+    "Sulfuras, Hand of Ragnaros",
+    ])
+    @special_items = special_items
+  end
+
+  def generic_item_check(item)
+    unless special?(item) 
+      item.sell_in >= 0 ? item.quality -= 1 : item.quality -= 2
+      item.quality = GEN_MIN if item.quality <= GEN_MIN
+    end
+  end
+
+  def aged_brie(item)
+    if item.name === 'Aged Brie'  
+      item.quality += 1 if item.sell_in >= 0
+      item.quality += 2 if item.sell_in.between?(-5, -1)  
+      item.quality += 3 if item.sell_in < -5
+      item.quality = GEN_MAX if item.quality >= GEN_MAX
+    end
+  end
+
+  def concert_pass(item)
+    if item.name === 'Backstage passes to a TAFKAL80ETC concert'
+      item.quality = 0 if item.sell_in === 0 
+      item.quality += 1 if item.sell_in > 10 
+      item.quality += 2 if item.sell_in.between?(6, 10)  
+      item.quality += 3 if item.sell_in.between?(1, 5) 
+      item.quality = GEN_MAX if item.quality >= GEN_MAX
+    end
+  end
+
+  def conjured(item)
+    if item.name.include?('Conjured')
+      item.sell_in >= 0 ? item.quality -= 2 : item.quality -= 4
+    end
+  end
+
+  def sulfuras(item)
+    if item.name === "Sulfuras, Hand of Ragnaros"
+      item.quality = SULFURAS_MAX
+      item.sell_in += 1
+    end
+  end
+
+  def special?(item)
+    @special_items.include?(item.name) || item.name.include?('Conjured')
+  end
+end
--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,57 +1,21 @@
 require_relative 'item'
+require_relative 'assessment'
 
 class GildedRose
-  SULFURAS_MAX = 80
-  GEN_MAX = 50
-  GEN_MIN = 0
 
-  def initialize(items)
+  def initialize(items, assess = ItemAssessor.new)
     @items = items
-    @arr = ['Aged Brie',
-      'Backstage passes to a TAFKAL80ETC concert',
-      "Sulfuras, Hand of Ragnaros",
-      ]
+    @assess = assess
   end
 
   def update_quality
     @items.each do |item|
       item.sell_in -= 1
-      quality_check(item) unless @arr.include?(item.name) || item.name.include?('Conjured')
-      conjured(item) if item.name.include?('Conjured')
-      aged_brie(item) if item.name === 'Aged Brie'  
-      concert_pass(item) if item.name === 'Backstage passes to a TAFKAL80ETC concert'
-      sulfuras(item) if item.name === "Sulfuras, Hand of Ragnaros" 
+      @assess.generic_item_check(item) 
+      @assess.conjured(item) 
+      @assess.aged_brie(item) 
+      @assess.concert_pass(item) 
+      @assess.sulfuras(item)  
     end
-  end
-
-  private
-
-  def quality_check(item)
-    item.sell_in >= 0 ? item.quality -= 1 : item.quality -= 2
-    item.quality = GEN_MIN if item.quality <= GEN_MIN
-  end
-
-  def aged_brie(item)
-    item.quality += 1 if item.sell_in >= 0
-    item.quality += 2 if item.sell_in.between?(-5, -1)  
-    item.quality += 3 if item.sell_in < -5
-    item.quality = GEN_MAX if item.quality >= GEN_MAX
-  end
-
-  def concert_pass(item)
-    item.quality = 0 if item.sell_in === 0 
-    item.quality += 1 if item.sell_in > 10 
-    item.quality += 2 if item.sell_in.between?(6, 10)  
-    item.quality += 3 if item.sell_in.between?(1, 5) 
-    item.quality = GEN_MAX if item.quality >= GEN_MAX
-  end
-
-  def sulfuras(item)
-    item.quality = SULFURAS_MAX
-    item.sell_in += 1
-  end
-
-  def conjured(item)
-      item.sell_in >= 0 ? item.quality -= 2 : item.quality -= 4
   end
 end
