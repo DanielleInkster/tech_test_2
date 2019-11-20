26eb740d4de4c2557ab53dc94039aafbfca19a07
DanielleInkster <danielle.inkster@gmail.com>
2019-11-20 17:09:08 +0000

    Refactor concert_pass based on feature test

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,6 +1,7 @@
 require_relative 'item'
 
 class GildedRose
+  SULFURAS_QUALITY = 80
   MAXIMUM_QUALITY = 50
   MINIMUM_QUALITY = 0
 
@@ -27,7 +28,7 @@ class GildedRose
 
   def quality_check(item)
     if item.quality ===  MINIMUM_QUALITY
-      item.quality
+      item.quality = MINIMUM_QUALITY
     else
       if item.sell_in >= 0
         item.quality -= 1
@@ -47,27 +48,26 @@ class GildedRose
         item.quality +=1
       end
     else 
-      item.quality
+      item.quality =MAXIMUM_QUALITY
     end
   end
 
   def concert_pass(item)
-    if item.quality < MAXIMUM_QUALITY
-      if item.sell_in > 10
+      if item.sell_in === 0
+        item.quality =0
+      elsif item.sell_in > 10 && item.quality <= (MAXIMUM_QUALITY - 1)
         item.quality +=1
-      elsif item.sell_in.between?(6,10)
+      elsif item.sell_in.between?(6,10) && item.quality <= (MAXIMUM_QUALITY - 2)
         item.quality +=2
-      elsif item.sell_in.between?(1,5)
+      elsif item.sell_in.between?(1,5) && item.quality <= (MAXIMUM_QUALITY - 3)
         item.quality +=3
       else 
-        item.quality = 0
-      end
-      else item.quality
+        item.quality = MAXIMUM_QUALITY
     end
   end
 
   def sulfuras(item)
-    item.quality
+    item.quality = SULFURAS_QUALITY
     item.sell_in +=1
   end
 end
